61432f756886af55607450e0de45dee845f47b89
yhr0710 <128389948+yhr0710@users.noreply.github.com>
2023-07-15 16:34:49 +0800

    Company

--- DBlogistics/src/main/java/com/system/service/EmployeeService.java
+++ DBlogistics/src/main/java/com/system/service/EmployeeService.java
@@ -5,8 +5,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
 * @author 21628
-* @description 针对表【employee】的数据库操作Service
-* @createDate 2023-07-15 00:24:24
+* @description 针对表【employee(员工信息)】的数据库操作Service
+* @createDate 2023-07-15 09:13:42
 */
 public interface EmployeeService extends IService<Employee> {
 
--- DBlogistics/src/test/java/com/system/DemoApplicationTests.java
+++ DBlogistics/src/test/java/com/system/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.system;
 
+import com.system.mapper.EmployeeMapper;
+import com.system.pojo.Employee;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
+@MapperScan("com.system.mapper")
 class DemoApplicationTests {
-
+    @Autowired
+    private EmployeeMapper employeeMapper;
     @Test
-    void contextLoads() {
+    public void select1(){
+        List<Employee> list= employeeMapper.selectList(null);
+        list.forEach(System.out::println);
     }
-
+    public void delete(){    //传入主键
+        employeeMapper.deleteById(8888);
+    }
+    public void deleteByMap(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("eId", 8885);
+    }
 }
